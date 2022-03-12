b331048a61ff13fe9356ef99618bd25bc8ea2c17
신병철 <mousecjf@gmail.com>
2022-03-12 22:53:45 +0900

    Add files via upload

--- core/test/lifecycle/NetworkClient.java
+++ core/test/lifecycle/NetworkClient.java
@@ -1,5 +1,9 @@
 package hello.core.lifecycle;
 
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -7,9 +11,6 @@ public class NetworkClient {
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-
     }
 
     public void setUrl(String url) {
@@ -28,5 +29,18 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    @PostConstruct
+    public void init() { // 이 메서드의 이름뜻은 의존관계 주입이 끝나면 호출해 주겠다 라는 뜻이다.
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() { // 이 메서드는 종료할 때 호출해 주는 메서드다.
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
